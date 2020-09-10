16dffbebd7b198e9d71808ba7bbab900c4524b37
Sindiso Mkhatshwa <mkhsin035@myuct.ac.za>
2020-09-10 03:41:30 +0200

    Created robot class interface

--- robot.h
+++ robot.h
@@ -13,7 +13,29 @@
 
 #ifndef ROBOT_H
 #define ROBOT_H
-
+#include <string> 
+#include <libplayerc/playerc.h>
+namespace mkhsin035{
+    class{
+    private:
+        std::string host; //address of the host computer goes here
+        int port; //the port this robot uses to connect to Player
+        int status; //1 if this robot is connected to player. 0 if this robot is offline
+        
+        playerc_client_t *robot;
+        playerc_position2d_t *p2d_proxy;
+        playerc_ranger_t *blob_proxy;
+        playerc_simulation_t *sim_proxy;
+    public:
+        robot();
+        robot(std::string host, int port);
+        int connect_to_server();
+        int subscribe_to_devices();
+        void enable_motors();
+        void request_geometries();
+        void shutdown();
+    };
+};
 
 
 #endif /* ROBOT_H */
