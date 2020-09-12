d067867f9faff8cbb0eb4a619405f27d3e767d35
Sindiso Mkhatshwa <mkhsin035@myuct.ac.za>
2020-09-12 22:24:35 +0200

    Made many changes in order to test system. Many changes made solely to test if class changes were working as expected

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@
 #include <cstdlib>
 #include "robot.h"
 #include "communication.h"
+#include <unistd.h>
+#include <vector>
 using namespace std;
 using namespace mkhsin035;
 /*
@@ -25,6 +27,7 @@ int main(int argc, char** argv) {
     robot robot2("localhost", 6666, "robot2");
     robot robot3("localhost", 6667, "robot3");
     robot robot4("localhost", 6668, "robot4");
+    
     //connect robots to server
     robot1.connect_to_server();
     robot2.connect_to_server();
@@ -36,17 +39,34 @@ int main(int argc, char** argv) {
     robot2.subscribe_to_devices();
     robot3.subscribe_to_devices();
     robot4.subscribe_to_devices();
-    
-//    //enable motors
+    cout<<"subscribing"<<endl;
+    playerc_simulation_t *sim_proxy;
+    sim_proxy = playerc_simulation_create(robot1.rob, 0);
+	  if (playerc_simulation_subscribe(sim_proxy, PLAYER_OPEN_MODE))
+                cout<<"could not be done"<<endl;
+    //enable motors
     robot1.enable_motors();
     robot2.enable_motors();
     robot3.enable_motors();
     robot4.enable_motors();
-//    //request geometries
+    //request geometries
     robot1.request_geometries();
     robot2.request_geometries();
     robot3.request_geometries();
     robot4.request_geometries();
+    
+    robot2.read_from_proxies();
+    pos p = robot2.get_position(sim_proxy);
+    cout<<"x: "<<p.x<<"\ny: "<<p.y<<"\nyaw: "<<p.yaw<<endl;
+    //Prepare comms
+    comms c;
+    c.add(robot1); c.add(robot2); c.add(robot3); c.add(robot4);
+    for (int i = 0; i < 1; i++){
+        robot1.read_from_proxies();robot2.read_from_proxies();robot3.read_from_proxies();robot4.read_from_proxies();
+        c.broadcast(robot1, sim_proxy);
+        usleep(1000000);
+    }
+    
     
     return 0;
 }
