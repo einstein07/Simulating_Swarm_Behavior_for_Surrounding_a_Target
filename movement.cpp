4d31b4276641437b89f1257db665fdcfc6eda27b
Sindiso Mkhatshwa <mkhsin035@myuct.ac.za>
2020-09-13 03:53:11 +0200

    Added move funtion definition

--- movement.cpp
+++ movement.cpp
@@ -15,9 +15,110 @@
 
 mkhsin035::movement::movement(double circle_diameter, double Dmin):circle_diameter(circle_diameter), min_dist_nearest(Dmin){}
 
-void mkhsin035::movement::move(robot& robo)
+void mkhsin035::movement::move(robot& robo, playerc_simulation_t *sim_proxy, double& forward_speed, double& turning_speed)
 {
+    double tspeed = 5;
+    int margin = 5;
+    mkhsin035::pos robo_pos = robo.get_position(sim_proxy);
+    if(robo.blob_proxy->blobs_count > 0)
+    {
+        robo.oil_spill_position_x = robo.blob_proxy->blobs[0].x;
+        robo.oil_spill_position_y = robo.blob_proxy->blobs[0].y;
+    }
+    if (robo.oil_spill_position_x != -1000)
+    {
         
+        double adjacent = robo.oil_spill_position_x - robo_pos.x;
+        double opp = robo.oil_spill_position_y - robo_pos.y;
+        double desired_yaw = tan(opp/adjacent);
+        
+        double Dt = dist(robo_pos.x, robo_pos.y, robo.oil_spill_position_x, robo.oil_spill_position_y);
+        if (Dt <= this->circle_diameter)
+        {
+            if(robo_pos.yaw > DTOR(desired_yaw + margin))
+            {
+                forward_speed = 0;
+                turning_speed = (-1) * tspeed;
+            }
+            else if (robo_pos.yaw < DTOR(desired_yaw - margin))
+            {
+                forward_speed = 0;
+                turning_speed = tspeed;
+            }
+            else
+            {
+                forward_speed = -0.1;
+                turning_speed = 0;
+            }
+            
+        }
+        else 
+        {
+          if(robo_pos.yaw > DTOR(desired_yaw + margin))
+            {
+                forward_speed = 0;
+                turning_speed = (-1) * tspeed;
+            }
+            else if (robo_pos.yaw < DTOR(desired_yaw - margin))
+            {
+                forward_speed = 0;
+                turning_speed = tspeed;
+            }
+            else
+            {
+                forward_speed = 0.1;
+                turning_speed = 0;
+            }  
+        }
+    }
+    else
+    {
+        double x_temp = 0.5 * (robo.nearest_robot_x + robo.farthest_robot_x);
+        double y_temp = 0.5 * (robo.nearest_robot_y + robo.farthest_robot_y);
+        double D_temp = dist(robo_pos.x, robo_pos.y, x_temp, y_temp);
+        
+        double adjacent = x_temp - robo_pos.x;
+        double opp = y_temp - robo_pos.y;
+        double desired_yaw = tan(opp/adjacent);
+        
+        if(D_temp <= this->circle_diameter)
+        {
+            if(robo_pos.yaw > DTOR(desired_yaw + margin))
+            {
+                forward_speed = 0;
+                turning_speed = (-1) * tspeed;
+            }
+            else if (robo_pos.yaw < DTOR(desired_yaw - margin))
+            {
+                forward_speed = 0;
+                turning_speed = tspeed;
+            }
+            else
+            {
+                forward_speed = -0.1;
+                turning_speed = 0;
+            }
+        }
+        else 
+        {
+          if(robo_pos.yaw > DTOR(desired_yaw + margin))
+            {
+                forward_speed = 0;
+                turning_speed = (-1) * tspeed;
+            }
+            else if (robo_pos.yaw < DTOR(desired_yaw - margin))
+            {
+                forward_speed = 0;
+                turning_speed = tspeed;
+            }
+            else
+            {
+                forward_speed = 0.1;
+                turning_speed = 0;
+            }  
+        }
+    }
+    
 }
 
       
@@ -62,4 +163,18 @@ void mkhsin035::movement::wander(robot& robo, double& forward_speed, double& tur
     t_speed = t_speed - max_turning_speed;
     forward_speed = f_speed;
     turning_speed = t_speed;
+}
+
+double mkhsin035::movement::dist(double xi, double yi, double xf, double yf)
+{
+    double x, y, dist; 
+            
+    //get distance from this robot to robot
+    x = xf - xi;
+    y = yf - yi;
+
+    //find euclidean distance from circle center to robot
+    dist = (x * x) + (y * y);
+    dist = sqrt(dist);
+    return dist;
 }
