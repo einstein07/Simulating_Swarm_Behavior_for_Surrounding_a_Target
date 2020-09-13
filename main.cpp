93250fc1335af7cc77586514a388a6961e492967
Sindiso Mkhatshwa <mkhsin035@myuct.ac.za>
2020-09-13 03:54:42 +0200

    Added code to test swarm performance, with regards to coordinated movements.

--- main.cpp
+++ main.cpp
@@ -57,7 +57,8 @@ int main(int argc, char** argv) {
     robot4.request_geometries();
     
     //useful variables 
-    double forward_speed, turning_speed;
+    double forward_speed, turning_speed, forward_speed2, turning_speed2,
+            forward_speed3, turning_speed3, forward_speed4, turning_speed4;
     srand(time(NULL));
     
     //Prepare comms
@@ -69,24 +70,65 @@ int main(int argc, char** argv) {
         robot1.read_from_proxies();robot2.read_from_proxies();robot3.read_from_proxies();robot4.read_from_proxies();
         //robot 1
         c.broadcast(robot1, sim_proxy);
+        if (robot1.blob_proxy->blobs_count == 0 && ((robot1.farthest_robot_x == -1000 || robot1.nearest_robot_x == -1000) && robot1.oil_spill_position_x == -1000))
+        {
+            moves.wander(robot1, forward_speed, turning_speed);
+            cout<<"robot 1: wandering"<<endl;
+        }
+        else
+        {
+            moves.move(robot1, sim_proxy, forward_speed, turning_speed);
+            c.broadcast(robot1, sim_proxy);
+            cout<<"robot 1: target spotted!"<<endl;
+        }
         moves.avoid_collisions(robot1, forward_speed, turning_speed);
-        moves.wander(robot1, forward_speed, turning_speed);
         robot1.set_motors(forward_speed, turning_speed);
+        
         //robot 2
         c.broadcast(robot2, sim_proxy);
-        moves.avoid_collisions(robot2, forward_speed, turning_speed);
-        moves.wander(robot2, forward_speed, turning_speed);
-        robot2.set_motors(forward_speed, turning_speed);
+        if (robot2.blob_proxy->blobs_count == 0 && ((robot2.farthest_robot_x == -1000 || robot2.nearest_robot_x == -1000) && robot2.oil_spill_position_x == -1000))
+        {
+            moves.wander(robot2, forward_speed2, turning_speed2);
+            cout<<"robot 2: wandering"<<endl;
+        }
+        else
+        {
+            moves.move(robot2, sim_proxy, forward_speed2, turning_speed2);
+            c.broadcast(robot2, sim_proxy);
+            cout<<"robot 2: target spotted!"<<endl;
+        }
+        moves.avoid_collisions(robot2, forward_speed2, turning_speed2);
+        robot2.set_motors(forward_speed2, turning_speed2);
+        
         //robot 3
         c.broadcast(robot3, sim_proxy);
-        moves.avoid_collisions(robot3, forward_speed, turning_speed);
-        moves.wander(robot3, forward_speed, turning_speed);
-        robot3.set_motors(forward_speed, turning_speed);
+        if(robot3.blob_proxy->blobs_count == 0 && ((robot3.farthest_robot_x == -1000 || robot3.nearest_robot_x == -1000) && robot3.oil_spill_position_x == -1000))
+        {
+            moves.wander(robot3, forward_speed3, turning_speed3);
+            cout<<"robot 3: wandering"<<endl;
+        }
+        else
+        {
+            moves.move(robot3, sim_proxy, forward_speed3, turning_speed3);
+            c.broadcast(robot3, sim_proxy);
+            cout<<"robot 3: target spotted!"<<endl;
+        }
+        moves.avoid_collisions(robot3, forward_speed3, turning_speed3);
+        robot3.set_motors(forward_speed3, turning_speed3);
+        
         //robot 4
         c.broadcast(robot4, sim_proxy);
-        moves.avoid_collisions(robot4, forward_speed, turning_speed);
-        moves.wander(robot4, forward_speed, turning_speed);
-        robot4.set_motors(forward_speed, turning_speed);
+        if(robot4.blob_proxy->blobs_count == 0 && ((robot4.farthest_robot_x == -1000 || robot4.nearest_robot_x == -1000) && robot4.oil_spill_position_x == -1000))
+        {
+            moves.wander(robot4, forward_speed4, turning_speed4);
+        }
+        else
+        {
+            moves.move(robot4, sim_proxy, forward_speed4, turning_speed4);
+            c.broadcast(robot4, sim_proxy);
+        }
+        moves.avoid_collisions(robot4, forward_speed4, turning_speed4);
+        robot4.set_motors(forward_speed4, turning_speed4);
         
         usleep(1000000);
     }
