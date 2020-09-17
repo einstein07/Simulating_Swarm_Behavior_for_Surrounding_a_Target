9e18eb97c7fd7411521538179ed7616afaa5a3a4
Sindiso Mkhatshwa <mkhsin035@myuct.ac.za>
2020-09-17 02:06:52 +0200

    Added code to decide initial coordinates to broadcast. Also addded code to decide when to broadcast self coordinates and when to broadcast received coordinates using a status variable. Also removed all distance calculating lines. Using static distance fn in movement class now

--- communication.cpp
+++ communication.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 
 #include "communication.h"
+#include "movement.h"
 //------------------------------------------------------------------------------
 //CODE
 //------------------------------------------------------------------------------
@@ -15,25 +16,42 @@
     void mkhsin035::comms::receive(mkhsin035::robot& robo, playerc_simulation_t *sim_proxy, double& bc_x, double& bc_y, double& oil_spill_position_x, double& oil_spill_position_y)
     {
         mkhsin035::pos robot_position = robo.get_position(sim_proxy);
-        double x = robot_position.x - bc_x;
-        double y = robot_position.y - bc_y;
-        double Dj = (x * x) + (y * y);
-        if(Dj > robo.Df)
+//        double x = robot_position.x - bc_x;
+//        double y = robot_position.y - bc_y;
+        double Dj = movement::dist(robot_position.x, robot_position.y, bc_x, bc_y);
+        //std::cout<<"Dj: "<<Dj<<std::endl;
+        //std::cout<<"Df: "<<robo.Df<<std::endl;
+        //std::cout<<"Dn: "<<robo.Dn<<std::endl;
+        if(robo.Df == INFINITY|| robo.Dn == INFINITY)
         {
             robo.farthest_robot_x = bc_x;
             robo.farthest_robot_y = bc_y;
-        }
-        else if(Dj < robo.Dn)
-        {
             robo.nearest_robot_x = bc_x;
             robo.nearest_robot_y = bc_y;
         }
-        robo.oil_spill_position_x = oil_spill_position_x;
-        robo.oil_spill_position_y = oil_spill_position_y;
-        robo.bc_x = bc_x;
-        robo.bc_y = bc_y;
+        else
+        {
+            if(Dj > robo.Df)
+            {
+                //std::cout<<"changed farthest"<<std::endl;
+                robo.farthest_robot_x = bc_x;
+                robo.farthest_robot_y = bc_y;
+            }
+            else if(Dj < robo.Dn)
+            {
+                //std::cout<<"changed nearest"<<std::endl;
+                robo.nearest_robot_x = bc_x;
+                robo.nearest_robot_y = bc_y;
+            }
+            robo.oil_spill_position_x = oil_spill_position_x;
+            robo.oil_spill_position_y = oil_spill_position_y;
+            robo.bc_x = bc_x;
+            robo.bc_y = bc_y;
+            robo.status = 1;
+        }
+        
     }
-void mkhsin035::comms::broadcast(robot& robo, playerc_simulation_t *sim_proxy)
+void mkhsin035::comms::broadcast(robot& robo, playerc_simulation_t *sim_proxy, std::vector<mkhsin035::robot> &robots)
 {
     //std::cout<<"broadcast:start"<<std::endl;
     mkhsin035::pos robot_position;
@@ -43,22 +61,24 @@ void mkhsin035::comms::broadcast(robot& robo, playerc_simulation_t *sim_proxy)
     //find the robot...
     robot_position = robo.get_position(sim_proxy);
     //Evaluate distance from the farthest known robot
-    double x_d = robot_position.x - robo.farthest_robot_x;
-    double y_d = robot_position.y - robo.farthest_robot_y;
-    robo.Df = (x_d * x_d) + (y_d * y_d);
-    robo.Df = sqrt(robo.Df);
+    robo.Df = robo.farthest_robot_x == INFINITY ? INFINITY : movement::dist(robot_position.x, robot_position.y, robo.farthest_robot_x, robo.farthest_robot_y);
     //Evaluate distance from the nearest known robot
-    x_d = robot_position.x - robo.nearest_robot_x;
-    y_d = robot_position.y - robo.nearest_robot_y;
-    robo.Dn = (x_d * x_d) + (y_d * y_d);
-    robo.Dn = sqrt(robo.Dn);
+    robo.Dn = robo.nearest_robot_x == INFINITY ? INFINITY : movement::dist(robot_position.x, robot_position.y, robo.nearest_robot_x, robo.nearest_robot_y);
+    if (robo.bc_x == INFINITY)
+    {
+        robo.status = 0; //broadcast own coordinates
+    }
+    
+    if(!robo.status)
+    {
+        robo.bc_x = robot_position.x;
+        robo.bc_y = robot_position.y;
+    }
     
     //get the poses of other robots in sim
     for(int i=0; i < 4; i++)
     {
-        robot r = robots[i];
-        other_robot_positions[i] = r.get_position(sim_proxy);
-        //std::cout<<"broadcast:pos for "<<i<<": "<<other_robot_positions[i].x<<" y: "<<other_robot_positions[i].y<<std::endl;
+        other_robot_positions[i] = robots[i].get_position(sim_proxy);
     }
     
     //find actual center relative to simulation.
@@ -67,26 +87,14 @@ void mkhsin035::comms::broadcast(robot& robo, playerc_simulation_t *sim_proxy)
            
     for(int i=0; i<4; i++)
         {
-            double x, y, dist; 
-            
-            //get distance from this robot to robot
-            x = circleX - other_robot_positions[i].x;
-            y = circleY - other_robot_positions[i].y;
-            
-            //find euclidean distance from circle center to robot
-            dist = (x * x) + (y * y);
-            dist = sqrt(dist);
+            double dist; 
+            dist = movement::dist(circleX, circleY, other_robot_positions[i].x, other_robot_positions[i].y);
                         
             if(dist > 0 && dist < radius)
             {
                 comms::receive(robots[i], sim_proxy, robo.bc_x, robo.bc_y, robo.oil_spill_position_x, robo.oil_spill_position_y);
-                std::cout<<"broadcasted "<<i<<std::endl;
+                robo.status = 0;
             }
         }
  
 }
-
-    void mkhsin035::comms::add(mkhsin035::robot& r)
-    {
-        robots.push_back(r);
-    }
