d596c1efe4a4dbadac719047eacd09e87803e924
Sindiso Mkhatshwa <mkhsin035@myuct.ac.za>
2020-09-17 02:31:38 +0200

    Added code to test all classes with regard to bug fixes performed.

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@
 #include "movement.h"
 #include <unistd.h>
 #include <vector>
+#include<chrono>
 using namespace std;
 using namespace mkhsin035;
 /*
@@ -28,6 +29,7 @@ int main(int argc, char** argv) {
     robot robot2("localhost", 6666, "robot2");
     robot robot3("localhost", 6667, "robot3");
     robot robot4("localhost", 6668, "robot4");
+    vector<robot> robots;
     
     //connect robots to server
     robot1.connect_to_server();
@@ -56,6 +58,11 @@ int main(int argc, char** argv) {
     robot3.request_geometries();
     robot4.request_geometries();
     
+    robots.push_back(robot1);
+    robots.push_back(robot2);
+    robots.push_back(robot3);
+    robots.push_back(robot4);
+    
     //useful variables 
     double forward_speed, turning_speed, forward_speed2, turning_speed2,
             forward_speed3, turning_speed3, forward_speed4, turning_speed4;
@@ -64,77 +71,83 @@ int main(int argc, char** argv) {
     //Prepare comms
     comms c;
     movement moves(3, 0.5);
-    c.add(robot1); c.add(robot2); c.add(robot3); c.add(robot4);
     while (true){
         //read from proxies
-        robot1.read_from_proxies();robot2.read_from_proxies();robot3.read_from_proxies();robot4.read_from_proxies();
+        robots[0].read_from_proxies();robots[1].read_from_proxies();robots[2].read_from_proxies();robots[3].read_from_proxies();
+//        moves.wander(robots[0], forward_speed, turning_speed);
+//        c.broadcast(robots[0], sim_proxy, robots);
+//        usleep(1000000);
+//        c.broadcast(robots[1], sim_proxy, robots);
+//        std::cout<<"robot 2 nearest robot coordinates: "<<robots[1].nearest_robot_x<<" "<<robots[1].nearest_robot_y<<std::endl;
+//        std::cout<<"robot 2 farthest robot coordinates: "<<robots[1].farthest_robot_x<<" "<<robots[1].farthest_robot_y<<std::endl;
+//        std::cout<<"robot 2 status"<<robots[1].status<<std::endl;
+//        usleep(1000000);
+//        c.broadcast(robots[3], sim_proxy, robots);
+//        usleep(1000000);
+//        std::cout<<"robot 2 status"<<robots[1].status<<std::endl;
         
- //       moves.move(robot1, sim_proxy, forward_speed, turning_speed);
-//        //robot 1
-        c.broadcast(robot1, sim_proxy);
+        //robot 1
+        c.broadcast(robots[0], sim_proxy, robots);
         cout<<"robot 1"<<endl;
-        cout<<"Nearest: "<<robot1.nearest_robot_x<<","<<robot1.nearest_robot_y<<endl;
-        if (robot1.blob_proxy->blobs_count == 0 && ((robot1.farthest_robot_x == -1000 || robot1.nearest_robot_x == -1000) && robot1.oil_spill_position_x == -1000))
+        if (robots[0].blob_proxy->blobs_count == 0 && ((robots[0].farthest_robot_x == INFINITY || robots[0].nearest_robot_x == INFINITY) && robots[0].oil_spill_position_x == INFINITY))
         {
-            moves.wander(robot1, forward_speed, turning_speed);
+            moves.wander(robots[0], forward_speed, turning_speed);
         }
         else
         {
-            moves.move(robot1, sim_proxy, forward_speed, turning_speed);
-            c.broadcast(robot1, sim_proxy);
+            moves.move(robots[0], sim_proxy, forward_speed, turning_speed);
+            c.broadcast(robots[0], sim_proxy, robots);
         }
-        moves.avoid_collisions(robot1, sim_proxy, forward_speed, turning_speed);
-        robot1.set_motors(forward_speed, turning_speed);
+        moves.avoid_collisions(robots[0], sim_proxy, forward_speed, turning_speed);
+        robots[0].set_motors(forward_speed, turning_speed);      
         
         //robot 2
-        c.broadcast(robot2, sim_proxy);
+        c.broadcast(robots[1], sim_proxy, robots);
         cout<<"robot 2"<<endl;
-        cout<<"Nearest: "<<robot2.nearest_robot_x<<","<<robot2.nearest_robot_y<<endl;
-        if (robot2.blob_proxy->blobs_count == 0 && ((robot2.farthest_robot_x == -1000 || robot2.nearest_robot_x == -1000) && robot2.oil_spill_position_x == -1000))
+        if (robots[1].blob_proxy->blobs_count == 0 && ((robots[1].farthest_robot_x == INFINITY || robots[1].nearest_robot_x == INFINITY) && robots[1].oil_spill_position_x == INFINITY))
         {
-            moves.wander(robot2, forward_speed2, turning_speed2);
+            moves.wander(robots[1], forward_speed2, turning_speed2);
         }
         else
         {
-            moves.move(robot2, sim_proxy, forward_speed2, turning_speed2);
-            c.broadcast(robot2, sim_proxy);
+            moves.move(robots[1], sim_proxy, forward_speed2, turning_speed2);
+            c.broadcast(robots[1], sim_proxy, robots);
         }
-        moves.avoid_collisions(robot2, sim_proxy, forward_speed2, turning_speed2);
-        robot2.set_motors(forward_speed2, turning_speed2);
+        moves.avoid_collisions(robots[1], sim_proxy, forward_speed2, turning_speed2);
+        robots[1].set_motors(forward_speed2, turning_speed2);        
         
-//        //robot 3
-        c.broadcast(robot3, sim_proxy);
+        //robot 3
+        c.broadcast(robots[2], sim_proxy, robots);
         cout<<"robot 3"<<endl;
-        cout<<"Nearest: "<<robot3.nearest_robot_x<<","<<robot3.nearest_robot_y<<endl;
-        if(robot3.blob_proxy->blobs_count == 0 && ((robot3.farthest_robot_x == -1000 || robot3.nearest_robot_x == -1000) && robot3.oil_spill_position_x == -1000))
+        if (robots[2].blob_proxy->blobs_count == 0 && ((robots[2].farthest_robot_x == INFINITY || robots[2].nearest_robot_x == INFINITY) && robots[2].oil_spill_position_x == INFINITY))
         {
-            moves.wander(robot3, forward_speed3, turning_speed3);
+            moves.wander(robots[2], forward_speed3, turning_speed3);
         }
         else
         {
-            moves.move(robot3, sim_proxy, forward_speed3, turning_speed3);
-            c.broadcast(robot3, sim_proxy);
+            moves.move(robots[2], sim_proxy, forward_speed3, turning_speed3);
+            c.broadcast(robots[2], sim_proxy, robots);
         }
-        moves.avoid_collisions(robot3, sim_proxy, forward_speed3, turning_speed3);
-        robot3.set_motors(forward_speed3, turning_speed3);
+        moves.avoid_collisions(robots[2], sim_proxy, forward_speed3, turning_speed3);
+        robots[2].set_motors(forward_speed3, turning_speed3);
         
         //robot 4
-        c.broadcast(robot4, sim_proxy);
+        c.broadcast(robots[3], sim_proxy, robots);
         cout<<"robot 4"<<endl;
-        cout<<"Nearest: "<<robot4.nearest_robot_x<<","<<robot4.nearest_robot_y<<endl;
-        if(robot4.blob_proxy->blobs_count == 0 && ((robot4.farthest_robot_x == -1000 || robot4.nearest_robot_x == -1000) && robot4.oil_spill_position_x == -1000))
+        if (robots[3].blob_proxy->blobs_count == 0 && ((robots[3].farthest_robot_x == INFINITY || robots[3].nearest_robot_x == INFINITY) && robots[3].oil_spill_position_x == INFINITY))
         {
-            moves.wander(robot4, forward_speed4, turning_speed4);
+            moves.wander(robots[3], forward_speed4, turning_speed4);
         }
         else
         {
-            moves.move(robot4, sim_proxy, forward_speed4, turning_speed4);
-            c.broadcast(robot4, sim_proxy);
+            moves.move(robots[3], sim_proxy, forward_speed4, turning_speed4);
+            c.broadcast(robots[3], sim_proxy, robots);
         }
-        moves.avoid_collisions(robot4, sim_proxy, forward_speed4, turning_speed4);
-        robot4.set_motors(forward_speed4, turning_speed4);
+        moves.avoid_collisions(robots[3], sim_proxy, forward_speed4, turning_speed4);
+        robots[3].set_motors(forward_speed4, turning_speed4);
+
         
-        usleep(1000000);
+        usleep(100000);
     }
     
     
