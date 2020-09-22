155e566481e68016b02d15287fabae2d9a90f275
Sindiso Mkhatshwa <mkhsin035@myuct.ac.za>
2020-09-23 00:59:04 +0200

    Added comments to class interface

--- robot.h
+++ robot.h
@@ -7,7 +7,6 @@
 /* 
  * File:   robot.h
  * Author: Sindiso Mkhatshwa
- *
  * Created on 10 September 2020, 02:58
  */
 
@@ -17,52 +16,75 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <functional>
 #include <bits/stdc++.h>
 #include <libplayerc/playerc.h>
 namespace mkhsin035{
+//------------------------------------------------------------------------------
+//struct used to store robot position
+//------------------------------------------------------------------------------
     struct Position
     {
-      double yaw;
-      double x;
-      double y;
+      double yaw;   //yaw of robot in simulation
+      double x;     //x position of robot in simulation     
+      double y;     //y position of robot in simulation
     }typedef pos;
-    
+//------------------------------------------------------------------------------
+//ROBOT CLASS
+//------------------------------------------------------------------------------    
     class robot{
     private:
-        char cfg_file_name[7];
-        char* host; //address of the host computer goes here
-        int port; //the port this robot uses to connect to Player                
-               
+        char cfg_file_name[7];//robot model name as it appears in cofig file
+        char* host;           //address of the host computer goes here
+        int port;             //the port no this robot uses to connect to Player                
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
     public:
-         
-        //Player proxies
-        playerc_ranger_t *sonar_proxy;
-        playerc_position2d_t *p2d_proxy;
-        playerc_blobfinder_t *blob_proxy;
-        playerc_client_t *rob;
-        //Comms
-        int status; //1 if this robot is supposed to broadcast received coordinates. 0 if this robot is supposed to receive its own coordinates
-        double bc_x; double bc_y; //broadcast coordinates
+        /*Player proxies*/
+        playerc_ranger_t *sonar_proxy;      //sonar proxy
+        playerc_position2d_t *p2d_proxy;    //position proxy
+        playerc_blobfinder_t *blob_proxy;   //blobfinder proxy
+        playerc_client_t *rob;              //client proxy
+//------------------------------------------------------------------------------
+        /*used to handle communications*/
+        int status;                                                             //1 if this robot is supposed to broadcast received coordinates. 0 if this robot is supposed to receive its own coordinates
+        //broadcast coordinates
+        double bc_x, bc_y;
+        //oil spill location coordinates
         double oil_spill_position_x; double oil_spill_position_y;
+        //nearest robot coordinates
         double nearest_robot_x; double nearest_robot_y;
+        //farthest robot coordinates
         double farthest_robot_x; double farthest_robot_y;
-        double Df; double Dn; //distance to farthest and nearest robots
-        
-        robot();
-        robot(char* host, int port, std::string name);
-        bool is_self(robot& other);
+        //distances to farthest and nearest robots
+        double Df; double Dn;
+//------------------------------------------------------------------------------
+        /*Constructors*/
+        robot();             //default constructor
+        robot(
+            char* host,      //address of the host computer            
+            int port,        //the port no. this robot uses to connect to Player
+            std::string name //robot model name as it appears on config file
+            );
+//------------------------------------------------------------------------------
+        /*function declarations*/
         int connect_to_server();
         int subscribe_to_devices();
         void enable_motors();
-        void set_motors(double& forward_speed, double& turning_speed);
+        void set_motors(
+                        double& forward_speed,//translational speed
+                        double& turning_speed //rotational speed  
+                    );
         void request_geometries();
         void read_from_proxies();
-        pos get_position(playerc_simulation_t *sim_proxy);
+        pos get_position(
+                        playerc_simulation_t *sim_proxy //sim proxy
+                    );
+        bool is_self(
+                        robot& other                    //robot
+                    );
         void shutdown();
     };
-          
 };
-
-
 #endif /* ROBOT_H */
 
